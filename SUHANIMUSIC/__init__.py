b18e0a28f4b471a346f42aa296b7847b4cea254b
˹Ҩ፝֟፝ɴ ꫝɴᴊᴀʟɪ˼ [🇮🇳] ♡゙ <laleshwarraj018@gmail.com>
2024-12-13 11:54:18 +0530

    ♡︎ ˹Ҩ፝֟፝ͷ ꫝɴᴊᴀʟɪ˼ [🇮🇳] ♡

--- SUHANIMUSIC/__init__.py
+++ SUHANIMUSIC/__init__.py
@@ -5,6 +5,7 @@ from SUHANIMUSIC.core.bot import SUHANI
 from SUHANIMUSIC.core.dir import dirr
 from SUHANIMUSIC.core.git import git
 from SUHANIMUSIC.core.userbot import Userbot
+from VIPMUSIC.core.youtube import SUHANI
 from SUHANIMUSIC.misc import dbb, heroku
 
 from SafoneAPI import SafoneAPI
@@ -14,6 +15,7 @@ dirr()
 git()
 dbb()
 heroku()
+SUHANI()
 
 app = SUHANI()
 api = SafoneAPI()
