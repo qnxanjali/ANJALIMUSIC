370e3cad39bd19650fba8fe9476b13040d044f1a
˹Ҩ፝֟፝ɴ ꫝɴᴊᴀʟɪ˼ [🇮🇳] ♡゙ <laleshwarraj018@gmail.com>
2024-12-13 14:57:17 +0530

    ♡︎ ˹Ҩ፝֟፝ͷ ꫝɴᴊᴀʟɪ˼ [🇮🇳] ♡

--- SUHANIMUSIC/utils/inline/play.py
+++ SUHANIMUSIC/utils/inline/play.py
@@ -59,50 +59,36 @@ def stream_markup_timer(_, chat_id, played, dur):
     else: 
         bar = "🅜︎🅐︎🅗︎🅣︎🅞—♥️—🅐︎🅝︎🅙︎🅐︎🅛︎🅘︎"
     buttons = [
-         [
-            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
-            InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
-            InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
-            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
-            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
-        ],
-        [
-            InlineKeyboardButton(
-                text=f"{played} {bar} {dur}",
-                callback_data="GetTimer",
-            )
-        ],
+         
           [
             InlineKeyboardButton(
-                text="✰ 𝖡ᴧ፝֠֩ʙꭎ ✰", url="https://t.me/chingchonggelichu",
+                text="✰ 𝖡ᴧ፝֠֩ʙꭎ ✰", url="https://t.me/Zhangweii",
             ),
             InlineKeyboardButton(
                 text="✰ 𝛅ᴏ፝֠֩𝛈ᴧ ✰", url="https://t.me/AnjaliOwnerBot",
             )
         ],
-          [ InlineKeyboardButton(text="ᴍᴀsᴛɪ ᴋɪ ʙᴀsᴛɪ", url=f"https://t.me/+b1gc4qrvfLZlNGI1")],
-    ]
+          [ InlineKeyboardButton(text="✰ ᴍᴀ፝֩֠֠֩sᴛɪ ᴋɪ ʙᴀ፝֠֩֠֩sᴛɪ ✰", url=f"https://t.me/+b1gc4qrvfLZlNGI1")],
+          [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
+        ]
     return buttons
 
 def stream_markup(_, chat_id):
     buttons = [
-        [
-            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
-            InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
-            InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
-            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
-            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
-         ],
+        
          [
             InlineKeyboardButton(
-                text="✰ 𝖡ᴧ፝֠֩ʙꭎ ✰", url="https://t.me/chingchonggelichu",
+                text="✰ 𝖡ᴧ፝֠֩ʙꭎ ✰", url="https://t.me/Zhangweii",
             ),
             InlineKeyboardButton(
                 text="✰ 𝛅ᴏ፝֠֩𝛈ᴧ ✰", url="https://t.me/AnjaliOwnerBot",
             )
         ],
-          [ InlineKeyboardButton(text="ᴍᴀsᴛɪ ᴋɪ ʙᴀsᴛɪ", url=f"https://t.me/+b1gc4qrvfLZlNGI1")],
+        [ InlineKeyboardButton(text="✰ ᴍᴀ፝֩֠֠֩sᴛɪ ᴋɪ ʙᴀ፝֠֩֠֩sᴛɪ ✰", url=f"https://t.me/+b1gc4qrvfLZlNGI1")],
+        [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
+    
     ]
+   
     return buttons
 
 
